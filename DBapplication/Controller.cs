c32dc0d9c3e188b08b3874afa4014b5de21170ba
Lenovo <Lenovo@DESKTOP-3N4TPI1>
2023-12-22 23:26:03 +0200

    Add project files.

---
+++ DBapplication/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace MaternalDatabase
+{
+    public class Controller
+    {
+        DBManager DBMan;
+        public Controller()
+        {
+            DBMan = new DBManager();
+        }
+        public void TerminateConnection()
+        {
+            DBMan.CloseConnection();
+        }
+    }
+}
